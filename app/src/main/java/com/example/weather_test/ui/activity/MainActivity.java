4d30b68dd6db3bd67c16eafc0e5a05aad2d2604e
alex <natalia42010822@mail.ru>
2023-07-28 12:59:48 +0300

    added pojo classes, viewmodel and api request

--- app/src/main/java/com/example/weather_test/ui/activity/MainActivity.java
+++ app/src/main/java/com/example/weather_test/ui/activity/MainActivity.java
@@ -1,25 +1,51 @@
 package com.example.weather_test.ui.activity;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
 import androidx.lifecycle.ViewModelProvider;
 
+import android.Manifest;
+import android.annotation.SuppressLint;
+import android.content.pm.PackageManager;
+import android.location.Address;
+import android.location.Geocoder;
+import android.location.Location;
 import android.os.Bundle;
+import android.service.autofill.SavedDatasetsInfoCallback;
+import android.text.style.StrikethroughSpan;
+import android.util.Log;
 
 import com.example.weather_test.R;
+import com.example.weather_test.api.response.WeatherResponse;
+import com.example.weather_test.api.services.ApiFactory;
 import com.example.weather_test.ui.viewmodel.ViewModel;
+import com.google.android.gms.location.FusedLocationProviderClient;
+import com.google.android.gms.location.LocationServices;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.gms.tasks.Task;
+
+import java.util.List;
+import java.util.Locale;
+
+import io.reactivex.rxjava3.android.schedulers.AndroidSchedulers;
+import io.reactivex.rxjava3.core.Scheduler;
+import io.reactivex.rxjava3.functions.Consumer;
+import io.reactivex.rxjava3.schedulers.Schedulers;
 
 public class MainActivity extends AppCompatActivity {
-    ViewModel viewModel;
 
+    ViewModel viewModel;
+    @SuppressLint("CheckResult")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        initViews();
 
         viewModel = new ViewModelProvider(this).get(ViewModel.class);
-    }
-    private void initViews(){
 
     }
 }
--- app/src/main/java/com/example/weather_test/ui/viewmodel/ViewModel.java
+++ app/src/main/java/com/example/weather_test/ui/viewmodel/ViewModel.java
@@ -1,16 +1,60 @@
 package com.example.weather_test.ui.viewmodel;
 
+import android.annotation.SuppressLint;
 import android.app.Application;
+import android.util.Log;
 
 import androidx.annotation.NonNull;
 import androidx.lifecycle.AndroidViewModel;
+import androidx.lifecycle.LiveData;
+import androidx.lifecycle.MutableLiveData;
 import androidx.lifecycle.ViewModelProvider;
 import androidx.lifecycle.ViewModelStore;
 
-public class ViewModel extends AndroidViewModel {
+import com.example.weather_test.api.response.WeatherResponse;
+import com.example.weather_test.api.services.ApiFactory;
+import com.example.weather_test.ui.activity.MainActivity;
+import com.google.android.gms.location.FusedLocationProviderClient;
+import com.google.android.gms.location.LocationServices;
 
+import io.reactivex.rxjava3.android.schedulers.AndroidSchedulers;
+import io.reactivex.rxjava3.functions.Consumer;
+import io.reactivex.rxjava3.schedulers.Schedulers;
+
+public class ViewModel extends AndroidViewModel {
+    private String TAG = "TAGTAGTAGTAG";
+    private final String API_KEY = "0ae590706091ef379c1aaeb379d4dad8";
+    FusedLocationProviderClient fusedLocationProviderClient;
+    private MutableLiveData<Double> latitude = new MutableLiveData<>();
+    private MutableLiveData<Double> longitude = new MutableLiveData<>();
+    public LiveData<Double> getLatitude() {
+        return latitude;
+    }
+    public LiveData<Double> getLongitude() {
+        return longitude;
+    }
 
     public ViewModel(@NonNull Application application) {
         super(application);
+        fusedLocationProviderClient = LocationServices.getFusedLocationProviderClient(application);
+
     }
+    @SuppressLint("CheckResult")
+    public void getWeatherDetails(double lat, double lon){
+        ApiFactory.apiService.loadWeather(lat,lon,API_KEY)
+                .subscribeOn(Schedulers.newThread())
+                .observeOn(AndroidSchedulers.mainThread())
+                .subscribe(new Consumer<WeatherResponse>() {
+                    @Override
+                    public void accept(WeatherResponse weatherResponse) throws Throwable {
+                        Log.d(TAG, "accept: "+ weatherResponse.toString());
+                    }
+                }, new Consumer<Throwable>() {
+                    @Override
+                    public void accept(Throwable throwable) throws Throwable {
+                        Log.d(TAG, throwable.toString());
+                    }
+                });
+    }
+
 }
