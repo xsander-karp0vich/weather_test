2b73bffba92ad9e00b20a703599fc05d207d3b89
alex <natalia42010822@mail.ru>
2023-07-30 22:09:20 +0300

    connected to the api, I get the correct data depending on the current location

--- app/src/main/java/com/example/weather_test/ui/activity/MainActivity.java
+++ app/src/main/java/com/example/weather_test/ui/activity/MainActivity.java
@@ -5,6 +5,7 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.app.ActivityCompat;
 import androidx.core.content.ContextCompat;
 import androidx.core.splashscreen.SplashScreen;
+import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProvider;
 
 import android.Manifest;
@@ -17,8 +18,10 @@ import android.os.Bundle;
 import android.service.autofill.SavedDatasetsInfoCallback;
 import android.text.style.StrikethroughSpan;
 import android.util.Log;
+import android.widget.Toast;
 
 import com.example.weather_test.R;
+import com.example.weather_test.api.pojo.Coordinates;
 import com.example.weather_test.api.response.WeatherResponse;
 import com.example.weather_test.api.services.ApiFactory;
 import com.example.weather_test.ui.viewmodel.ViewModel;
@@ -40,14 +43,68 @@ import io.reactivex.rxjava3.schedulers.Schedulers;
 public class MainActivity extends AppCompatActivity {
 
     ViewModel viewModel;
-    @SuppressLint("CheckResult")
+    private static int REQUEST_CODE = 100;
+
+    private final String TAG = "TAGTAGTAGTAG";
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-        SplashScreen splashScreen = SplashScreen.installSplashScreen(this);
+        SplashScreen splashScreen = SplashScreen.installSplashScreen(this); //SplashScreen
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
         viewModel = new ViewModelProvider(this).get(ViewModel.class);
 
+        askPermission(); //calling a method that requests permission
+
+        observeViewModel();
     }
+
+    private void askPermission(){
+        if (ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION)
+                == PackageManager.PERMISSION_GRANTED){
+            // If the permission has already been granted, call the method to get the user's current location.
+            viewModel.getCurrentUserLocation();
+
+        } else {
+            // If permission is not granted, request it from the user.
+            ActivityCompat.requestPermissions(this,
+                    new String[]{Manifest.permission.ACCESS_FINE_LOCATION},REQUEST_CODE);
+        }
+
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        if (requestCode == REQUEST_CODE){
+            if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                viewModel.getCurrentUserLocation();
+            }
+            else {
+               // Toast.makeText(this, "Something went wrong", Toast.LENGTH_SHORT).show();
+            }
+        }
+    }
+
+    private void observeViewModel(){
+        viewModel.getCoordinates().observe(this, new Observer<Coordinates<Double, Double>>() {
+            @Override
+            public void onChanged(Coordinates<Double, Double> coordinates) {
+                Log.d(TAG, "LOCATION: "+coordinates.toString());
+                viewModel.getWeatherDetails(coordinates);
+            }
+        });
+
+    }
+    private void updateUI(){
+        viewModel.getWeatherResponseLiveData().observe(this, new Observer<WeatherResponse>() {
+            @Override
+            public void onChanged(WeatherResponse weatherResponse) {
+                
+            }
+        });
+    }
+
 }
--- app/src/main/java/com/example/weather_test/ui/viewmodel/ViewModel.java
+++ app/src/main/java/com/example/weather_test/ui/viewmodel/ViewModel.java
@@ -1,48 +1,108 @@
 package com.example.weather_test.ui.viewmodel;
 
+import android.Manifest;
 import android.annotation.SuppressLint;
+import android.app.Activity;
 import android.app.Application;
+import android.content.Context;
+import android.content.pm.PackageManager;
+import android.location.Address;
+import android.location.Geocoder;
+import android.location.Location;
 import android.util.Log;
 
 import androidx.annotation.NonNull;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
 import androidx.lifecycle.AndroidViewModel;
 import androidx.lifecycle.LiveData;
 import androidx.lifecycle.MutableLiveData;
 import androidx.lifecycle.ViewModelProvider;
 import androidx.lifecycle.ViewModelStore;
 
+import com.example.weather_test.api.pojo.Coordinates;
 import com.example.weather_test.api.response.WeatherResponse;
 import com.example.weather_test.api.services.ApiFactory;
 import com.example.weather_test.ui.activity.MainActivity;
 import com.google.android.gms.location.FusedLocationProviderClient;
 import com.google.android.gms.location.LocationServices;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+
+import java.io.IOException;
+import java.util.List;
+import java.util.Locale;
 
 import io.reactivex.rxjava3.android.schedulers.AndroidSchedulers;
+import io.reactivex.rxjava3.core.Single;
 import io.reactivex.rxjava3.functions.Consumer;
 import io.reactivex.rxjava3.schedulers.Schedulers;
+import retrofit2.Call;
+import retrofit2.Response;
 
 public class ViewModel extends AndroidViewModel {
     private String TAG = "TAGTAGTAGTAG";
     private final String API_KEY = "0ae590706091ef379c1aaeb379d4dad8";
     FusedLocationProviderClient fusedLocationProviderClient;
-    private MutableLiveData<Double> latitude = new MutableLiveData<>();
-    private MutableLiveData<Double> longitude = new MutableLiveData<>();
 
+    private MutableLiveData<WeatherResponse> weatherResponseLiveData = new MutableLiveData<>();
+    private MutableLiveData<Coordinates<Double,Double>> coordinates = new MutableLiveData<>();
+    public LiveData<Coordinates<Double, Double>> getCoordinates() {
+        return coordinates;
+    }
+
+    public MutableLiveData<WeatherResponse> getWeatherResponseLiveData() {
+        return weatherResponseLiveData;
+    }
 
     public ViewModel(@NonNull Application application) {
         super(application);
         fusedLocationProviderClient = LocationServices.getFusedLocationProviderClient(application);
+    }
+
+    public void getCurrentUserLocation(){
+        if (ContextCompat.checkSelfPermission(getApplication(), Manifest.permission.ACCESS_FINE_LOCATION)== PackageManager.PERMISSION_GRANTED){
+            fusedLocationProviderClient.getLastLocation()
+                    .addOnSuccessListener(new OnSuccessListener<Location>() {
+                        @Override
+                        public void onSuccess(Location location) {
+                            if (location != null) {
+                                Geocoder geocoder = new Geocoder(getApplication(), Locale.getDefault());
+                                try {
+                                    List<Address> addresses = geocoder.getFromLocation(location.getLatitude(), location.getLongitude(), 1);
+
+                                    double lat = addresses.get(0).getLatitude();
+                                    double lon = addresses.get(0).getLongitude();
+
+                                    coordinates.setValue(new Coordinates<>(lat,lon));
+
+                                } catch (IOException e) {
+                                    throw new RuntimeException(e);
+                                }
+
+                            }
+                        }
+                    });
+        }
+
 
     }
+
     @SuppressLint("CheckResult")
-    public void getWeatherDetails(double lat, double lon){
+    public void getWeatherDetails(Coordinates coordinates){
+        double lat = (double) coordinates.getLatitude();
+        double lon = (double) coordinates.getLongitude();
+
+
         ApiFactory.apiService.loadWeather(lat,lon,API_KEY)
                 .subscribeOn(Schedulers.newThread())
                 .observeOn(AndroidSchedulers.mainThread())
                 .subscribe(new Consumer<WeatherResponse>() {
                     @Override
                     public void accept(WeatherResponse weatherResponse) throws Throwable {
-                        Log.d(TAG, "accept: "+ weatherResponse.toString());
+                        Log.d(TAG, "REQUEST: "+ weatherResponse.toString());
+                        weatherResponseLiveData.setValue(weatherResponse);
+
                     }
                 }, new Consumer<Throwable>() {
                     @Override
@@ -52,4 +112,8 @@ public class ViewModel extends AndroidViewModel {
                 });
     }
 
+    @Override
+    protected void onCleared() {
+        super.onCleared();
+    }
 }
